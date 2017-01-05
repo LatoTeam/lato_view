487cb1b66bfb42b7a93712d1cf1250a08e294642
Riccardo-Zanutta <zanuttar@gmail.com>
2017-01-05 14:18:08 +0100

    Content block + Nav update shadow, Building Step component

---
+++ app/assets/javascripts/lato_view/build/Step.js
@@ -0,0 +1,30 @@
+var Step = (function($) {
+  'use strict';
+
+  var $stepElements = $('.element-step');
+
+  var _buildNavigation = function() {
+
+  };
+
+  var _validateStep = function() {
+
+  };
+
+  var _prevStep = function() {
+
+  };
+
+  var _nextStep = function() {
+
+  };
+
+  var init = function() {
+
+  };
+
+  return {
+    init: init
+  };
+
+})(jQuery);

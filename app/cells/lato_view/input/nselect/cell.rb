8de3e33395a21362309540171689071057a8feb5
Antoniox22 <antoniox22@live.it>
2017-01-12 11:51:50 +0100

    nselect beta v1, not tested

---
+++ app/cells/lato_view/input/nselect/cell.rb
@@ -0,0 +1,47 @@
+module LatoView
+  class Input::Nselect::Cell < Cell
+
+    @@widths = VIEW_INPUTWIDTH
+
+    attr_accessor :width, :required,
+                  :disabled, :custom_class,
+                  :select_inputs
+               
+    def initialize(width: 'large', required: false, disabled: false,
+                   custom_class: '', select_inputs:[] )
+
+      # save params
+      @width = width
+      @required = required
+      @disabled = disabled
+      @custom_class = custom_class
+
+      # for each inputs we will have: name, label, api_url, value, value_field, label_field
+      @select_inputs = select_inputs
+
+
+      # check params
+      check_params
+    end
+
+    def show
+      render "show.html"
+    end
+
+    # return required string to input
+    protected def required_data_input
+      return "data-input='required'" if @required
+    end
+
+    # return disabled string to input
+    protected def disabled_data_input
+      return "data-input='disabled'" if @disabled
+    end
+
+    # check params
+    private def check_params
+      raise 'Input Cells: width has not a correct value' unless @@widths.include? width
+    end
+
+  end
+end

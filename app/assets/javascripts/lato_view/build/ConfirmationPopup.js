8f6f7165eee213521ad64de5dbe18b7574ce0889
Riccardo-Zanutta <zanuttar@gmail.com>
2017-01-27 14:25:01 +0100

    Confirmation Button

--- app/assets/javascripts/lato_view/build/ConfirmationPopup.js
+++ app/assets/javascripts/lato_view/build/ConfirmationPopup.js
@@ -2,13 +2,43 @@ var ConfirmationPopup = (function($) {
   'use strict';
 
   var _manageConfirmationPopup = function() {
+    $(document).on('click', '.js-confirmation-popup-trigger', function(event) {
+      var $this = $(this);
 
-    
+      var $confirmationPopup = $('.js-confirmation-popup');
+      var $confirmationPopupQuestion = $confirmationPopup.find('.js-confirmation-popup-question');
+      var $confirmationPopupAnswerOk = $confirmationPopup.find('.js-confirmation-popup-answer-success');
+      var $confirmationPopupAnswerNope = $confirmationPopup.find('.js-confirmation-popup-answer-nope');
 
+      var location = $this.attr('href');
+      var messageOk = $this.data('popup-ok');
+      var messageNo = $this.data('popup-no');
+      var messageText = $this.data('popup-text');
+
+      // Set property data
+      $confirmationPopupQuestion.text(messageText);
+      $confirmationPopupAnswerOk.text(messageOk);
+      $confirmationPopupAnswerOk.attr('href', location);
+      $confirmationPopupAnswerNope.text(messageNo);
+
+      event.preventDefault();
+
+      setTimeout(function() {
+        $('body').addClass('is-confirmation-popup-open');
+      }, 300);
+
+    });
+  };
+
+  var _nopePopup = function() {
+    $(document).on('click', '.js-confirmation-popup-answer-nope', function() {
+      $('body').removeClass('is-confirmation-popup-open');
+    });
   };
 
   var init = function() {
     _manageConfirmationPopup();
+    _nopePopup();
   };
 
   return {

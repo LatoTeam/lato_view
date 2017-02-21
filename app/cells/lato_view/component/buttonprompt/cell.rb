a59e715f2eb90892b0baa6e96222601eb20e1b80
Antonio Dal Cin <Ax22@dalz.local>
2017-02-21 19:22:25 +0100

    Added prompt button component

---
+++ app/cells/lato_view/component/buttonprompt/cell.rb
@@ -0,0 +1,36 @@
+module LatoView
+  class Component::Buttonprompt::Cell < Cell
+
+    @@colors = %w(normal yellow green red)
+    @@sizes = %w(normal small xsmall)
+
+    attr_accessor :title, :url, :color, :size, :custom_class, :data_popup_text, :data_popup_no, :data_popup_ok
+
+    def initialize(title: '', url: '', color: 'normal', size: 'normal', 
+                   custom_class: '', data_popup_text: '', 
+                   data_popup_no: '', data_popup_ok: '')
+     # save params
+      @title = title
+      @url = url
+      @color = color
+      @size = size
+      @custom_class = custom_class
+      @data_popup_text = data_popup_text
+      @data_popup_no = data_popup_no
+      @data_popup_ok = data_popup_ok
+      # check params
+      check_params
+    end
+
+    def show
+      render 'show.html'
+    end
+
+    # check params
+    private def check_params
+      raise 'Button Cell: color value in not correct' unless @@colors.include? @color.to_s
+      raise 'Button Cell: size value in not correct' unless @@sizes.include? @size.to_s
+    end
+
+  end
+end

f9bb8d2b40456929628bf9bdb9ccd0d5887f6366
Gregorio Galante <gregogalante@gmail.com>
2017-02-22 20:16:58 +0100

    Update assets.rb

--- lib/lato_view/interface/assets.rb
+++ lib/lato_view/interface/assets.rb
@@ -41,7 +41,7 @@ module LatoView
         config = YAML.load(
           File.read(File.expand_path("#{directory}/view.yml", __FILE__))
         )
-        key = (config && config['google_maps_key'] ? config['google_maps_key'] : nill)
+        key = (config && config['google_maps_key'] ? config['google_maps_key'] : nil)
       end
       # ritorno il risultato
       return key
